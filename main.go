e90bf919ef4bf067e3611b3da565859cebce9f9c
Ayrton Vitor <ayrtonvitor@pm.me>
2025-08-01 03:52:52 -0300

    setsup db from main

--- main.go
+++ main.go
@@ -1,14 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"os"
 
+	"github.com/ayrtonvitor/argumentative/internal/database"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("ERROR: .env unreadable: %v", err)
 	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatalln("ERROR: DATABASE_URL environment variable is not set")
+	}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("ERROR: Unnable to cannect to database: %v", err)
+	}
+
+	dbQueries := database.New(db)
+
+	cfg := apiConfig{dbQueries}
 }
