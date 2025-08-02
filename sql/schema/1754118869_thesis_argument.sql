4c899f978a0cf97c96747826e6196413f8d2fa1d
Ayrton Vitor <ayrtonvitor@pm.me>
2025-08-02 04:22:19 -0300

    create join table between thesis and argument

---
+++ sql/schema/1754118869_thesis_argument.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE thesis_argument (
+	thesis_id UUID NOT NULL,
+	argument_id UUID NOT NULL,
+	PRIMARY KEY (thesis_id, argument_id),
+	FOREIGN KEY (thesis_id)
+		REFERENCES thesis(id)
+		ON DELETE CASCADE,
+	FOREIGN KEY (argument_id)
+		REFERENCES argument(id)
+		ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE thesis_argument;
