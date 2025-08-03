89056b704d9d181b22f179a59b327cebb6f4ebbd
Ayrton Vitor <ayrtonvitor@pm.me>
2025-08-03 00:48:39 -0300

    import create argument handler

--- argument_handlers.go
+++ argument_handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 
+	"github.com/ayrtonvitor/argumentative/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -12,16 +14,45 @@ func (cfg *apiConfig) handleArgumentCreation(w http.ResponseWriter, r *http.Requ
 		Brief       string    `json:"brief"`
 		Description string    `json:"description"`
 		ThesisId    uuid.UUID `json:"thesisId"`
-		Sources     []string  `json:"sources"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameterns{}
-	err := decoder.Decode(&decoder)
+	err := decoder.Decode(&params)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not create argument", err)
 		return
 	}
 
-	created, err := cfg.queries
+	tx, err := cfg.db.Begin()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create argument", err)
+		return
+	}
+	defer tx.Rollback()
+
+	qtx := cfg.queries.WithTx(tx)
+	createArgumentParams := database.CreateArgumentParams{
+		Brief:       params.Brief,
+		Description: sql.NullString{params.Description, true},
+	}
+	created, err := qtx.CreateArgument(r.Context(), createArgumentParams)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create argument", err)
+		return
+	}
+
+	insertJointThesisParams := database.InsertTheisJoinArgumentParams{params.ThesisId, created.ID}
+	err = qtx.InsertTheisJoinArgument(r.Context(), insertJointThesisParams)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create argument", err)
+		return
+	}
+	err = tx.Commit()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create argument", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, created)
 }
