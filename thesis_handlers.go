385a9d9f1ffb5f0c131fc6005c6953e590f4caea
Ayrton Vitor <ayrtonvitor@pm.me>
2025-08-05 01:36:49 -0300

    add handler to list thesis

--- thesis_handlers.go
+++ thesis_handlers.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/ayrtonvitor/argumentative/internal/database"
+	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handleThesisCreation(w http.ResponseWriter, r *http.Request) {
@@ -33,4 +35,32 @@ func (cfg *apiConfig) handleThesisCreation(w http.ResponseWriter, r *http.Reques
 	}
 
 	respondWithJSON(w, http.StatusCreated, created)
+}
+
+func (cfg *apiConfig) handleGetThesisById(w http.ResponseWriter, r *http.Request) {
+	const queryParamName = "thesis_id"
+	const idListSep = ","
+	rawThesisIds := r.URL.Query().Get(queryParamName)
+	if rawThesisIds == "" {
+		respondWithError(w, http.StatusBadRequest, "Thesis id must be non-empty", nil)
+		return
+	}
+
+	thesisIds := make([]uuid.UUID, 0)
+	for _, rawThesisId := range strings.Split(rawThesisIds, idListSep) {
+		thesisId, err := uuid.Parse(rawThesisId)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Thesis id is malformed", err)
+			return
+		}
+		thesisIds = append(thesisIds, thesisId)
+	}
+
+	created, err := cfg.queries.GetThesisById(r.Context(), thesisIds)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not get thesis", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, created)
 }
